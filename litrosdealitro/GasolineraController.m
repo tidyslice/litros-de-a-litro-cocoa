9f1bbaea0b0cf76eb32d4c181a9ed6fbc4ba645a
Erick <ecamacho.mx@gmail.com>
2011-08-06 19:45:23 -0500

    added segmented control to gasolinera view

--- litrosdealitro/GasolineraController.m
+++ litrosdealitro/GasolineraController.m
@@ -45,10 +45,9 @@
 
 -(IBAction)togglePanels:(id)sender {
     UISegmentedControl *toggleControl = (UISegmentedControl *) sender;
-    
+
     infoView.hidden = toggleControl.selectedSegmentIndex != 0;
     mapaView.hidden = toggleControl.selectedSegmentIndex == 0;
-    
 }
 
 #pragma mark - View lifecycle
@@ -57,6 +56,16 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    UISegmentedControl *toggleViewControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects: @"Info", @"Mapa", nil]];
+    [toggleViewControl addTarget:self action:@selector(togglePanels:) forControlEvents:UIControlEventValueChanged];
+    toggleViewControl.selectedSegmentIndex  = 0;
+    toggleViewControl.segmentedControlStyle = UISegmentedControlStyleBar;
+    
+    UIBarButtonItem *toggleViewBarItem = [[UIBarButtonItem alloc] initWithCustomView:toggleViewControl];
+    self.navigationItem.rightBarButtonItem = toggleViewBarItem;
+    [toggleViewControl release];
+    [toggleViewBarItem release];
+    
 }
 
 - (void)viewDidUnload
